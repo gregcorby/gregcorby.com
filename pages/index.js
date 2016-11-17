1ae895a0ea442062cbbb0eb464981a8745d7b7f2
Greg Corby <greg.corby@gmail.com>
2016-11-17 12:58:41 -0800

    home page fix

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,15 @@ export default class Sass extends React.Component {
           <DocumentTitle title={ 'Greg Corby' }></DocumentTitle>
 
           <div className="row sm-row hero-text">
-          <h2 className="intro"><span>Greg Corby is a <br />sun-drenched, <br />salty designer</span></h2>
+          <h2 className="intro"><span>Greg Corby is a <br />sun-drenched, <br />salty designer.</span></h2>
           </div>
           
           <div className="about">
             <div className="row sm-row">
                 <div className="hero-image column sm-column-50"></div>
                 <div className="about-copy column sm-column-50">
-                  <p>I’m Greg Corby, a designer working in Southern California. I help brands & startups create digital stuff that’s not overly-complicated and looks nice, too.</p>
-                  <p>If you're interested in working together, <Link to={prefixLink('/contact/')}>shoot me a message,</Link> or find me on <a href="http://www.dribbble.com/gregcorby" target="_blank">Dribbble</a>, <a href="http://www.instagram.com/gregcorby" target="_blank">Instagram</a>, <a href="http://www.twitter.com/gregcorby" target="_blank">Twitter</a>, & <a href="http://www.facebook.com/gregcorby" target="_blank">Facebook</a>.</p>            
+                  <p>I help brands make digital things that<br />aren't over-complicated and look nice.</p>
+                  <Link to={prefixLink('/contact/')}>Let's work together</Link>
                 </div>
             </div>
             </div>      
@@ -37,8 +37,8 @@ export default class Sass extends React.Component {
       container.classList.add('loaded')
     }, 300);
     setTimeout(function() {
-      container.classList.add('loaded-two')
-    }, 3000);
+      container.classList.add('loaded-text')
+    }, 1200);
   }
   componentWillUnmount() {
     var container = document.getElementById('content-container');
